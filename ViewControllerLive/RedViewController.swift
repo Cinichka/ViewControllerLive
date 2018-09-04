6d6ee167cf42fd583dcbe76f458d741460451b3a
cinichka85 <cinichka85@gmail.com>
2018-09-04 15:06:51 +0300

    first commit

---
+++ ViewControllerLive/RedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RedViewController.swift
+//  ViewControllerLive
+//
+//  Created by Вероника Садовская on 04.09.2018.
+//  Copyright © 2018 Veronika Sadovskaya. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
