f40c7f431ab5f8868e176ec7d2bceb1412e35c9f
cinichka85 <cinichka85@gmail.com>
2018-09-04 09:40:38 +0300

    Initial Commit

---
+++ ViewControllerLive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerLive
+//
+//  Created by Вероника Садовская on 04.09.2018.
+//  Copyright © 2018 Veronika Sadovskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
